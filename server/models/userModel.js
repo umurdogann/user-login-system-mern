8430b12c30743db633bd1d826418d20f35824b5a
Cuma Umur Doğan <umurdogann@gmail.com>
2021-10-27 00:55:10 +0300

    user model completed

---
+++ server/models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model("user",userSchema);
