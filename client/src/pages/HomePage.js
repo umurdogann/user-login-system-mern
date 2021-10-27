ac7a805b852fa9f35ae48caf0a61f947e9aa5677
Cuma Umur Doğan <umurdogann@gmail.com>
2021-10-27 05:24:40 +0300

    homepage designed

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div style={{textAlign:"center"}}>
+            This route is <b>protected route</b>            
+        </div>
+    )
+}
+
+export default HomePage
