03608f5047607af39109c82f79b1947200312a95
Cuma Umur Doğan <umurdogann@gmail.com>
2021-10-27 05:25:56 +0300

    protected route and react router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import { useContext } from "react";
+import { userContext } from "./contexts/userContext/userContext";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
+  const { user, dispatch } = useContext(userContext);
+  console.log(user);
   return (
-    <div>
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {!user ? (
+          <div>
+            <Link to="/login">Login</Link>
+            <span> | </span>
+            <Link to="/register">Register</Link>
+          </div>
+        ) : (
+          <div style={{width:"100%"}}>
+            <div style={{display:"inline-block",width:"100%", textAlign:"center"}} >
+              <p style={{fontWeight: "bold"}}>MERN STACK LOGIN SYSTEM</p>
+              </div>
+            <div style={{position: "absolute", display:"inline-block",  left:"92vw"}}>
+              <button onClick={()=>dispatch({type:"LOGOUT"})} style= {{border: "1px solid black",backgroundColor:"white",cursor:"pointer", fontSize:"2.3rem"}}>Logout</button>
+              </div>
+          </div>
+        )}
+      </div>
+      <hr />
+      <Switch>
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <ProtectedRoute
+          exact
+          path="/home"
+          component={HomePage}
+          isAuth={user ? true : false}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
