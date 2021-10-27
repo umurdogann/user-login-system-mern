04f4f076807d154c0282a49a765ec001d071865a
Cuma Umur Doğan <umurdogann@gmail.com>
2021-10-27 17:55:42 +0300

    favicon and title edited

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch, Redirect } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
@@ -26,18 +26,49 @@ function App() {
             <Link to="/register">Register</Link>
           </div>
         ) : (
-          <div style={{width:"100%"}}>
-            <div style={{display:"inline-block",width:"100%", textAlign:"center"}} >
-              <p style={{fontWeight: "bold"}}>MERN STACK LOGIN SYSTEM</p>
-              </div>
-            <div style={{position: "absolute", display:"inline-block",  left:"92vw"}}>
-              <button onClick={()=>dispatch({type:"LOGOUT"})} style= {{border: "1px solid black",backgroundColor:"white",cursor:"pointer", fontSize:"2.3rem"}}>Logout</button>
-              </div>
+          <div style={{ width: "100%" }}>
+            <div
+              style={{
+                display: "inline-block",
+                width: "100%",
+                textAlign: "center",
+              }}
+            >
+              <p style={{ fontWeight: "bold" }}>MERN STACK LOGIN SYSTEM</p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                display: "inline-block",
+                left: "92vw",
+              }}
+            >
+              <button
+                onClick={() => dispatch({ type: "LOGOUT" })}
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "white",
+                  cursor: "pointer",
+                  fontSize: "2.3rem",
+                }}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         )}
       </div>
       <hr />
       <Switch>
+        <Route exact path="/">
+          <div style={{ textAlign: "center" }}>
+            {!user ? (
+              <p>Please login or register</p>
+            ) : (
+              <p>You already loged in</p>
+            )}
+          </div>
+        </Route>
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/register" component={RegisterPage} />
         <ProtectedRoute
